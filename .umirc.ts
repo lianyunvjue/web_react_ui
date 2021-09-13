d874a44bea228effaca502d64e7fc7b8881ba5b4
Nicky <heishaluo@163.com>
2021-09-13 17:24:58 +0800

    init commit

--- .umirc.ts
+++ .umirc.ts
@@ -4,9 +4,23 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  // 配置开启布局插件
   layout: {},
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  //接入antd@4.0
+  antd: {},
+  // 使用 dva 数据流
+  dva: {
+    immer: true,
+    hmr: false,
+  },
+  //使用代理跨域请求
+  proxy: {
+    '/api': {
+      target: 'https://a.b.com/',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from './index.less';
 
 export default function IndexPage() {
   return (
-    <div>
+    <div className={styles.page}>
       <h1 className={styles.title}>Page index</h1>
     </div>
   );
